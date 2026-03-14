dc2e62d73f6e8bdefcba9d1cf252575e8c3aedd7
tiagoaaestrada <tiago.estrada@hotmail.com>
2026-03-14 01:33:25 +0000

    Create page.tsx

---
+++ app/animals/page.tsx
@@ -0,0 +1,11 @@
+export default function Animals() {
+  return (
+    <main style={{ padding: "40px", fontFamily: "Arial" }}>
+      <h1>Animais</h1>
+
+      <p>Lista de animais</p>
+
+      <button>+ Novo animal</button>
+    </main>
+  )
+}
