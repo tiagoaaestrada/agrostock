8d7af65e7c2bb35a63fd8d955139c146b78a054d
tiagoaaestrada <tiago.estrada@hotmail.com>
2026-03-14 01:33:46 +0000

    Create page.tsx

---
+++ app/parcels/page.tsx
@@ -0,0 +1,11 @@
+export default function Parcels() {
+  return (
+    <main style={{ padding: "40px", fontFamily: "Arial" }}>
+      <h1>Parcelas</h1>
+
+      <p>Gestão das pastagens</p>
+
+      <button>+ Nova parcela</button>
+    </main>
+  )
+}
